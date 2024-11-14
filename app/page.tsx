4c4bed6647cff3baf78874046cbe85b273dca696
Gabriel Godoy <gabyreload@gmail.com>
2024-11-14 17:32:30 -0300

    grid to layout socialmedia and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,100 +1,64 @@
-import Image from "next/image";
+import Link from "next/link";
+import {
+  FaDownload,
+  FaGithub,
+  FaInstagram,
+  FaLinkedin,
+  FaPhone,
+  FaWhatsapp,
+} from "react-icons/fa";
 
 export default function Home() {
+  const socialMedia = {
+    linkedin: "https://www.linkedin.com/in/estebangabrielgodoy/",
+    github: "https://github.com/gabyreload1983/",
+    whatsapp: "https://wa.me/+543476643800",
+    instagram: "https://www.instagram.com/estebangabrielgodoy/",
+    cv: "https://gabrielgodoy.com.ar/resources/cv/estebangabrielgodoy.pdf",
+    celphone: "+5493476643800",
+  };
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div className="h-screen bg-gradient-to-r from-purple-500 to-pink-500 grid grid-rows-3">
+      <nav className="flex justify-center items-center">
+        <ul className="flex flex-col items-center gap-3">
+          <Link
+            href="/"
+            className="bg-black py-3 px-10 w-full rounded-md hover:bg-pink-700 hover:text-black"
           >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+            <li className="text-1xl text-center">PROYECTS</li>
+          </Link>
+          <Link
+            href="/"
+            className="bg-black py-3 px-10 w-full rounded-md hover:bg-pink-700 hover:text-black"
           >
-            Read our docs
-          </a>
-        </div>
+            <li className="text-1xl text-center">CONTACT</li>
+          </Link>
+        </ul>
+      </nav>
+      <main className="flex flex-col justify-center items-center">
+        <h1 className="text-5xl text-black">Gabriel Godoy</h1>
+        <h2 className="text-3xl">SYSTEMS ANALYST</h2>
+        <h3 className="text-xl">WEB DEVELOPMENT</h3>
       </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+      <footer className="flex justify-center items-end gap-5 mb-5 text-2xl lg:text-4xl">
+        <Link href={socialMedia.linkedin} target="_blank">
+          <FaLinkedin className="hover:fill-black" />
+        </Link>
+        <Link href={socialMedia.github} target="_blank">
+          <FaGithub className="hover:fill-black" />
+        </Link>
+        <Link href={socialMedia.whatsapp} target="_blank">
+          <FaWhatsapp className="hover:fill-black" />
+        </Link>
+        <Link href={socialMedia.instagram} target="_blank">
+          <FaInstagram className="hover:fill-black" />
+        </Link>
+        <Link href={socialMedia.cv} target="_blank">
+          <FaDownload className="hover:fill-black" />
+        </Link>
+        <Link href={socialMedia.celphone} target="_blank">
+          <FaPhone className="hover:fill-black" />
+        </Link>
       </footer>
     </div>
   );
