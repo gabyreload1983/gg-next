7a1e443cc5e102adccae4b22a9b57a786ab039b7
Gabriel Godoy <gabyreload@gmail.com>
2025-08-13 09:41:47 -0300

    update cv url

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
     github: "https://github.com/gabyreload1983/",
     whatsapp: "https://wa.me/+543476643800",
     instagram: "https://www.instagram.com/estebangabrielgodoy/",
-    cv: "/cv/estebangabrielgodoy.pdf",
+    cv: "https://drive.google.com/file/d/1428Wnfd8YzBz0O03KlX_nkaYuoUvM5Si/view?usp=drive_link",
     celphone: "tel:+5493476643800",
     calendar: "https://calendly.com/gabyreload",
   };
